433a6b810f694c293ef4797cac87b6a0dea5a803
Abhinav Prasai <abhinav.prsai@gmail.com>
2025-10-27 14:43:58 +0000

    open display

--- xpet.c
+++ xpet.c
@@ -7,9 +7,28 @@
 
 void setup(void);
 
+Display* dpy = NULL;
+Window root;
+
+int scr;
+int scr_width;
+int scr_height;
+int revert_to;
+
+Bool running = False;
+
 void setup(void)
 {
+	dpy = XOpenDisplay(NULL);
+	if (!dpy) {
+		fprintf(stderr, "xpet: failed to open display\n");
+		exit(EXIT_FAILURE);
+	}
 
+	scr = XDefaultScreen(dpy);
+	scr_width = XDisplayWidth(dpy, scr);
+	scr_height = XDisplayHeight(dpy, scr);
+	root = RootWindow(dpy, scr);
 }
 
 int main(int argc, char** argv)
@@ -17,8 +36,9 @@ int main(int argc, char** argv)
 	(void) argv;
 	if (argc > 1) {
 		printf(
-			"xpets "VERSION
-			"");
+			"xpets "VERSION"\r\n"
+			"uint 2025"
+		);
 	}
 
 	setup();
--- xpet.h
+++ xpet.h
@@ -1,3 +1,11 @@
 #pragma once
 
+#include <X11/Xlib.h>
+
 #define VERSION "v0.1"
+
+struct {
+	Window window;
+	char* name;
+	char* message;
+} pet;
